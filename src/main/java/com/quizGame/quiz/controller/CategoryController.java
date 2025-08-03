8f47caae84b83414717dd4a7453349dd25e47800
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 00:17:58 +0545

    refactor category controller logic

--- src/main/java/com/quizGame/quiz/controller/CategoryController.java
+++ src/main/java/com/quizGame/quiz/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package com.quizGame.quiz.controller;
 
+import com.quizGame.quiz.model.Category;
+import com.quizGame.quiz.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping("/addCategory")
+    public void addCategory(@RequestParam String categoryName, @RequestParam String categoryDescription){
+        Category category = new Category();
+        category.setCategoryName(categoryName);
+        category.setCategoryDescription(categoryDescription);
+        categoryService.insertCategory(category);
+    }
 }
