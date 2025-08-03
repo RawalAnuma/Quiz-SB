abbbdab3eb609ffb3feff55a749333416ead064f
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 00:01:02 +0545

    attributes and getter/setter method created

--- src/main/java/com/quizGame/quiz/model/Category.java
+++ src/main/java/com/quizGame/quiz/model/Category.java
@@ -1,4 +1,53 @@
 package com.quizGame.quiz.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Timestamp;
+
+@Entity
+
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+
+    private String categoryName;
+    private String categoryDescription;
+    private Timestamp createdDate;
+
+    public Category() {
+    }
+    public Category(String categoryName, String categoryDescription) {
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+        this.createdDate = new Timestamp(System.currentTimeMillis());
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
 }
