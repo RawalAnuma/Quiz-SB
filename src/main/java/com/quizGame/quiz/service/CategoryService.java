fd76083057e47d53304950a25ecc977cdc61b666
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 00:17:10 +0545

    method for saving category is created

--- src/main/java/com/quizGame/quiz/service/CategoryService.java
+++ src/main/java/com/quizGame/quiz/service/CategoryService.java
@@ -1,4 +1,17 @@
 package com.quizGame.quiz.service;
 
+import com.quizGame.quiz.model.Category;
+import com.quizGame.quiz.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public void insertCategory(Category category){
+        categoryRepository.save(category);
+    }
 }
