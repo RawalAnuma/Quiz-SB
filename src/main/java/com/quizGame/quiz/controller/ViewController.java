48bdd946bcf9921a49032dd86b6e164641c75a13
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 00:18:55 +0545

    displaying concept is applied

--- src/main/java/com/quizGame/quiz/controller/ViewController.java
+++ src/main/java/com/quizGame/quiz/controller/ViewController.java
@@ -1,4 +1,24 @@
 package com.quizGame.quiz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ViewController {
+
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "home";
+    }
+
+    @GetMapping("/signup")
+    public String showSignupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
 }
