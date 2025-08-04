ed03b7c45ca117dc957d7ccce8daad2bc66c693a
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 21:58:08 +0545

    interface created extending Jpa repo

--- src/main/java/com/quizGame/quiz/repository/QuizRepository.java
+++ src/main/java/com/quizGame/quiz/repository/QuizRepository.java
@@ -1,7 +1,11 @@
 package com.quizGame.quiz.repository;
 
 import com.quizGame.quiz.model.Quiz;
+import com.quizGame.quiz.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+    List<Quiz> findByUserId(User user);
 }
