e031083a7e48879fc34f4ec390bd27fdc83297a2
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 21:57:00 +0545

    method created to get quiz by user id

---
+++ src/main/java/com/quizGame/quiz/service/QuestionService.java
@@ -0,0 +1,4 @@
+package com.quizGame.quiz.service;
+
+public class QuestionService {
+}
--- src/main/java/com/quizGame/quiz/service/QuizService.java
+++ src/main/java/com/quizGame/quiz/service/QuizService.java
@@ -1,17 +1,23 @@
 package com.quizGame.quiz.service;
 
 import com.quizGame.quiz.model.Quiz;
+import com.quizGame.quiz.model.User;
 import com.quizGame.quiz.repository.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class QuizService {
+public class  QuizService {
 
     @Autowired
     private QuizRepository quizRepository;
 
     public void insertQuiz(Quiz quiz){
         quizRepository.save(quiz);
+    }
+    public List<Quiz> getQuizByUserId(User user) {
+        return quizRepository.findByUserId(user);
     }
 }
