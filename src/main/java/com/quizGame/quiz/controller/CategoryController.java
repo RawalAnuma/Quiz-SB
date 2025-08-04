b56d9bde5c8cc03cdb311843d1501741b5e90c53
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 12:52:39 +0545

    methods added for getting all categories and getting category by id

--- src/main/java/com/quizGame/quiz/controller/CategoryController.java
+++ src/main/java/com/quizGame/quiz/controller/CategoryController.java
@@ -4,9 +4,14 @@ import com.quizGame.quiz.model.Category;
 import com.quizGame.quiz.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/categories")
@@ -21,5 +26,23 @@ public class CategoryController {
         category.setCategoryName(categoryName);
         category.setCategoryDescription(categoryDescription);
         categoryService.insertCategory(category);
+    }
+
+    @GetMapping("/getAllCategories")
+    public String getAllCategories(Model model){
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "createQuiz";
+    }
+
+    @GetMapping("/getCategoryById")
+    public Category getCategoryById(@RequestParam int categoryId) {
+        Optional<Category> category = categoryService.getCategoryById(categoryId);
+        if (category.isPresent()) {
+            return category.get();
+        } else {
+            throw new RuntimeException("Category not found with id: " + categoryId);
+        }
+
     }
 }
