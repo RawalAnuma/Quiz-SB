47fab2530304d3ea5a0ce2935e8c39a03aa7f26c
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 12:50:45 +0545

    method added to find category by id

---
+++ src/main/java/com/quizGame/quiz/repository/QuizRepository.java
@@ -0,0 +1,4 @@
+package com.quizGame.quiz.repository;
+
+public interface QuizRepository {
+}
--- src/main/java/com/quizGame/quiz/service/CategoryService.java
+++ src/main/java/com/quizGame/quiz/service/CategoryService.java
@@ -5,6 +5,9 @@ import com.quizGame.quiz.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoryService {
 
@@ -13,5 +16,14 @@ public class CategoryService {
 
     public void insertCategory(Category category){
         categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(int categoryId) {
+        return categoryRepository.findById(categoryId);
+
     }
 }
