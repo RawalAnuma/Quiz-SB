6e68e14741907e6e3d431db3fec5fc32de921499
RawalAnuma <rawalanuma@gmail.com>
2025-08-04 13:28:22 +0545

    session used to store data

--- src/main/java/com/quizGame/quiz/controller/QuizController.java
+++ src/main/java/com/quizGame/quiz/controller/QuizController.java
@@ -1,4 +1,38 @@
 package com.quizGame.quiz.controller;
 
+import com.quizGame.quiz.model.Category;
+import com.quizGame.quiz.model.Quiz;
+import com.quizGame.quiz.model.User;
+import com.quizGame.quiz.service.QuizService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/quizzes")
 public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    @Autowired
+    private CategoryController categoryController;
+
+    @PostMapping("/addQuiz")
+    public void createQuiz(@RequestParam String quizName, @RequestParam String quizDescription, @RequestParam int noOfQuestionsToPlay, @RequestParam int categoryId, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        Category category = categoryController.getCategoryById(categoryId);
+        Quiz quiz = new Quiz();
+        quiz.setQuizName(quizName);
+        quiz.setQuizDescription(quizDescription);
+        quiz.setNoOfQuestionsToPlay(noOfQuestionsToPlay);
+        quiz.setCategory(category);
+        quiz.setUser(user);
+        quizService.insertQuiz(quiz);
+    }
 }
