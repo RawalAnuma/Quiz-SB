c53b008ca005298f2bbb02e0ddc264ed5231c487
RawalAnuma <rawalanuma@gmail.com>
2025-08-06 11:54:36 +0545

    updated user adding annotation

--- src/main/java/com/quizGame/quiz/model/User.java
+++ src/main/java/com/quizGame/quiz/model/User.java
@@ -42,21 +42,22 @@ public class User {
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-    public boolean getIsAdmin(){
-        return isAdmin;
-    }
-    public void setGameMaster(boolean isGameMaster){
-        this.isAdmin = isAdmin;
-    }
+
 
     public boolean isAdmin() {
-        return isAdmin();
+        return isAdmin;
     }
 
     public void setAdmin(boolean isAdmin) {
