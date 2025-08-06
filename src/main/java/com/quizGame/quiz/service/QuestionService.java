02bad7b7ede3dd0b1b82f118dc9d0fdffe5645e9
RawalAnuma <rawalanuma@gmail.com>
2025-08-06 12:00:55 +0545

    method created to get questions by quiz id

--- src/main/java/com/quizGame/quiz/service/QuestionService.java
+++ src/main/java/com/quizGame/quiz/service/QuestionService.java
@@ -1,4 +1,20 @@
 package com.quizGame.quiz.service;
 
+import com.quizGame.quiz.model.Question;
+import com.quizGame.quiz.model.Quiz;
+import com.quizGame.quiz.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public List<Question> getQuestionsByQuizId(Quiz quiz) {
+        return questionRepository.findByQuiz(quiz);
+    }
 }
