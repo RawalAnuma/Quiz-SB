4aa42472bc8e2dbeff77427f0961885dd702e575
RawalAnuma <rawalanuma@gmail.com>
2025-08-06 12:05:20 +0545

    updated getQuestionsByQuizID with proper mapping and path varaibale

--- src/main/java/com/quizGame/quiz/controller/QuestionController.java
+++ src/main/java/com/quizGame/quiz/controller/QuestionController.java
@@ -1,4 +1,33 @@
 package com.quizGame.quiz.controller;
 
+import com.quizGame.quiz.model.Question;
+import com.quizGame.quiz.model.Quiz;
+import com.quizGame.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/questions")
 public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private QuizController quizController;
+
+    @GetMapping("/getQuestions/{quizId}")
+    public String getQuestionsByQuizID(@PathVariable int quizId, Model model){
+        Quiz quiz = quizController.getQuizById(quizId);
+        List<Question> questions = questionService.getQuestionsByQuizId(quiz);
+        model.addAttribute("questions", questions);
+        return "questions";
+    }
+
 }
