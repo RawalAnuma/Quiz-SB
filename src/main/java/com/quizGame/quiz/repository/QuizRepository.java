a8a1fe07f7c0434c5999e7fb29ff7f95269ff685
RawalAnuma <rawalanuma@gmail.com>
2025-08-06 11:58:24 +0545

    created quiz repo

--- src/main/java/com/quizGame/quiz/repository/QuizRepository.java
+++ src/main/java/com/quizGame/quiz/repository/QuizRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
-    List<Quiz> findByUserId(User user);
+    List<Quiz> findByUser(User user);
 }
