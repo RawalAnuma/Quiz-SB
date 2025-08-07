d5e4fda4018eeada804f43baa4643464c537a493
RawalAnuma <rawalanuma@gmail.com>
2025-08-07 19:20:26 +0545

    helps to view updated question page

--- src/main/java/com/quizGame/quiz/controller/ViewController.java
+++ src/main/java/com/quizGame/quiz/controller/ViewController.java
@@ -1,10 +1,17 @@
 package com.quizGame.quiz.controller;
 
+import com.quizGame.quiz.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewController {
+
+    @Autowired
+    private QuestionController questionController;
 
     @GetMapping("/home")
     public String showHomePage() {
@@ -19,6 +26,19 @@ public class ViewController {
     @GetMapping("/login")
     public String showLoginPage() {
         return "login";
+    }
+
+    @GetMapping("/addQuestionView/{quizId}")
+    public String showAddQuestionPage(@PathVariable int quizId, Model model) {
+        model.addAttribute("quizId", quizId);
+        return "insertQuestions";
+    }
+
+    @GetMapping("/updateQuestionView/{questionId}")
+    public String showUpdateQuestionPage(@PathVariable int questionId, Model model){
+        Question question = questionController.getQuestionById(questionId);
+        model.addAttribute("question", question);
+        return "updateQuestion";
     }
 
 }
