7ad2a4ac95d7391d1546b976a1c6c1c68fc32a2c
RawalAnuma <rawalanuma@gmail.com>
2025-08-15 23:07:19 +0545

    scores finding by user

--- src/main/java/com/quizGame/quiz/repository/ScoreRepository.java
+++ src/main/java/com/quizGame/quiz/repository/ScoreRepository.java
@@ -1,7 +1,12 @@
 package com.quizGame.quiz.repository;
 
 import com.quizGame.quiz.model.Score;
+import com.quizGame.quiz.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
+
+    List<Score> findByUser(User user);
 }
