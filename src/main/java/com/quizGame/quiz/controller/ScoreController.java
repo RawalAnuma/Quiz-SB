2a834605c5dff1677dbaf93f3af134907b0b68b4
RawalAnuma <rawalanuma@gmail.com>
2025-08-16 00:02:36 +0545

    chnaged post mapping to get mapping

--- src/main/java/com/quizGame/quiz/controller/ScoreController.java
+++ src/main/java/com/quizGame/quiz/controller/ScoreController.java
@@ -9,7 +9,9 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +21,7 @@ public class ScoreController {
     @Autowired
     private ScoreService scoreService;
 
+    @GetMapping("/saveScore/{quizId}")
     public String saveScore(@PathVariable int quizId, HttpSession session, Model model){
         Integer finalScore = (Integer) session.getAttribute("score");
         if(finalScore == null) {
@@ -37,8 +40,16 @@ public class ScoreController {
         score.setPlayedDate(java.sql.Date.valueOf(java.time.LocalDate.now()));
         scoreService.saveScore(score);
 
+        session.removeAttribute("score");
         model.addAttribute("score", finalScore);
         return "scorePage"; // Redirect to a page to display the score
 
+    }
+
+    @GetMapping("/myScores")
+    public String myScores(HttpSession session, Model model) {
+        User currentUser = (User) session.getAttribute("user");
+        model.addAttribute("scores", scoreService.getScoresByUser(currentUser));
+        return "myScores"; // Redirect to a page to display the user's scores
     }
 }
