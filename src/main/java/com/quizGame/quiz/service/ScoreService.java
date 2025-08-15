f97b5639ca6d198a7cd7b907d889f3f7e691d490
RawalAnuma <rawalanuma@gmail.com>
2025-08-15 23:07:57 +0545

    get list of scores

--- src/main/java/com/quizGame/quiz/service/ScoreService.java
+++ src/main/java/com/quizGame/quiz/service/ScoreService.java
@@ -1,4 +1,24 @@
 package com.quizGame.quiz.service;
 
+import com.quizGame.quiz.model.Score;
+import com.quizGame.quiz.model.User;
+import com.quizGame.quiz.repository.ScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ScoreService {
+
+    @Autowired
+    private ScoreRepository scoreRepository;
+
+    public void saveScore(Score score){
+        scoreRepository.save(score);
+    }
+
+    public List<Score> getScoresByUser(User user){
+        return scoreRepository.findByUser(user);
+    }
 }
