eb54fef3b4829a42082bb8e2073a273afb53f8d9
RawalAnuma <rawalanuma@gmail.com>
2025-08-17 23:08:53 +0545

    get all scores in desc

--- src/main/java/com/quizGame/quiz/repository/ScoreRepository.java
+++ src/main/java/com/quizGame/quiz/repository/ScoreRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
 
     List<Score> findByUser(User user);
+
+    List<Score> findAllByOrderByScoreDesc();
 }
